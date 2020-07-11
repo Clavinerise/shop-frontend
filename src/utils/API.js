f81c7ad0282d5df3325d177d3a72fb2d553ed0f6
Jasmine <chan.jasmine4@gmail.com>
2020-07-12 01:25:00 +0800

    change baseurl for rest api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://10.140.17.148:3000/api/v1",
+  baseURL: "https://jasminechan.xyz:3001/api/v1",
   responseType: "json"
 });
